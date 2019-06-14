c59c3179f565cde518371d922768fd2159339a68
n894779 <lucio.passos@santander.com.br>
2019-06-14 20:23:00 -0300

    resolucao de conflitos

--- src/test/java/santander/autosavings/middleware/GoalTests.java
+++ src/test/java/santander/autosavings/middleware/GoalTests.java
@@ -1,8 +1,28 @@
 package santander.autosavings.middleware;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.santander.autosavings.middleware.model.Goal;
+import com.santander.autosavings.middleware.model.TransactionGoal;
+import com.santander.autosavings.middleware.service.GoalService;
+import com.santander.autosavings.middleware.utils.UrlRest;
 
 
 @SpringBootConfiguration()
