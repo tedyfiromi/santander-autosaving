4921f390634ae57e376892c9d3bae03e6bdafa64
n894779 <lucio.passos@santander.com.br>
2019-06-14 20:22:29 -0300

    resolucao de conflitos

---
+++ src/test/java/santander/autosavings/middleware/TransactionTests.java
@@ -0,0 +1,18 @@
+package santander.autosavings.middleware;
+
+import org.junit.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+@SpringBootConfiguration
+@SpringBootTest
+public class TransactionTests {
+	
+	@Test
+	public void testTransactionCreate() {
+		
+	}
+
+}
